ef75cfd76a6ddeceb154055c229c625bbd728b65
Saikumar Molleti <80804527+saikumarmolleti@users.noreply.github.com>
2023-01-07 21:26:15 +0530

    Done

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch(
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+)
+  .then((coinData) => {
+    // console.log(coindata);
+    return coinData.json(); // converted to object
+  })
+  .then((coinDataObject) => {
+    // console.log(coinDataObject);
+    let table_data = '';
+    coinDataObject.map((coin) => {
+      table_data += `<tr>
+     <td class="coin"><img src="${coin.image}">${coin.name}</td>
+     <td class="symbol">${coin.symbol}</td>
+     <td class="price" >$${coin.current_price}</td>
+     <td class="total" >$${coin.market_cap.toLocaleString()}</td>
+     <td class="per" > ${Number.parseFloat(
+       coin.price_change_percentage_24h
+     ).toFixed(2)}% </td>
+     <td class="cap" >Mkt Cap: ${coin.total_volume}</td>
+     </tr>`;
+    });
+    document.getElementById('tableData').innerHTML = table_data;
+  });
